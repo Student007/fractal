0c93c6984549f35490d2063274a96af48169083b
Tyler Stone <tyler.stone@emc.com>
2015-02-04 17:54:10 -0500

    added comment

--- app/routes/frontend.js
+++ app/routes/frontend.js
@@ -2,7 +2,7 @@
 
 module.exports = function(app) {
 
-    route to handle all angular requests
+    //route to handle all angular requests
     app.get('/', function(req, res) {
         res.sendfile('./public/views/index.html'); // load our public/index.html file
     });
