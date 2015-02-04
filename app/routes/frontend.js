ab41bd5d3bde65882623751094c2930a8efaa0db
Tyler Stone <tyler.stone@emc.com>
2015-02-04 17:51:05 -0500

    modified server

--- app/routes/frontend.js
+++ app/routes/frontend.js
@@ -2,8 +2,8 @@
 
 module.exports = function(app) {
 
-    // route to handle all angular requests
-    app.get('*', function(req, res) {
+    route to handle all angular requests
+    app.get('/', function(req, res) {
         res.sendfile('./public/views/index.html'); // load our public/index.html file
     });
 };
