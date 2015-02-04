69aa71842d94172e130e860594a15b89e7f56965
Tyler Stone <tyler.stone@emc.com>
2015-02-04 16:55:02 -0500

    updated for dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,4 +92,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', ['clean', 'compass', 'cssmin', 'jshint', 'ngAnnotate', 'uglify', 'concurrent']);
+  grunt.registerTask('heroku', ['clean', 'compass', 'cssmin', 'jshint', 'ngAnnotate', 'uglify', 'concurrent']);
 };
